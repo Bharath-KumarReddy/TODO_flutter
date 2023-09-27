8ef59010ea797ea45dfd8d466953575a32f32352
Bharath-KumarReddy <127650446+Bharath-KumarReddy@users.noreply.github.com>
2023-09-27 23:48:12 +0530

    Update Edit.dart

--- lib/Edit.dart
+++ lib/Edit.dart
@@ -1,19 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get/get.dart';
-import 'package:usingfirebase/Home.dart';
+import 'package:flutter/material.dart';
 
 class Edit extends StatefulWidget {
   final String taskId;
   final String initialTitle;
   final String initialDescription;
+  final String initialTaskType;
+  final String initialCategory;
 
-  const Edit({
-    Key? key,
+  Edit({
     required this.taskId,
     required this.initialTitle,
     required this.initialDescription,
-  }) : super(key: key);
+    required this.initialTaskType,
+    required this.initialCategory,
+  });
 
   @override
   State<Edit> createState() => _EditState();
@@ -22,6 +23,8 @@ class Edit extends StatefulWidget {
 class _EditState extends State<Edit> {
   late TextEditingController titleController;
   late TextEditingController descriptionController;
+  String selectedTaskType = '';
+  String selectedCategory = '';
 
   @override
   void initState() {
@@ -29,39 +32,35 @@ class _EditState extends State<Edit> {
     titleController = TextEditingController(text: widget.initialTitle);
     descriptionController =
         TextEditingController(text: widget.initialDescription);
+    selectedTaskType = widget.initialTaskType;
+    selectedCategory = widget.initialCategory;
   }
 
-  @override
-  void dispose() {
-    titleController.dispose();
-    descriptionController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _updateTask() async {
-    final title = titleController.text;
-    final description = descriptionController.text;
-
+  void updateTask() async {
     try {
-      await FirebaseFirestore.instance
-          .collection('tasks')
-          .doc(widget.taskId)
-          .update({
-        'title': title,
-        'description': description,
+      CollectionReference todos =
+          FirebaseFirestore.instance.collection('todos');
+
+      await todos.doc(widget.taskId).update({
+        'title': titleController.text,
+        'description': descriptionController.text,
+        'type': selectedTaskType,
+        'category': selectedCategory,
       });
 
-      Get.snackbar(
-        'Success',
-        'Task updated successfully',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.green,
-        colorText: Colors.white,
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Task updated successfully.'),
+        ),
       );
 
-      Get.to(() => Home());
-    } catch (error) {
-      print('Error updating task: $error');
+      Navigator.of(context).pop();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to update the task: $e'),
+        ),
+      );
     }
   }
 
@@ -71,26 +70,175 @@ class _EditState extends State<Edit> {
       appBar: AppBar(
         title: Text('Edit Task'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: titleController,
-              decoration: InputDecoration(labelText: 'Title'),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: descriptionController,
-              decoration: InputDecoration(labelText: 'Description'),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _updateTask,
-              child: Text('Update Task'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
+        padding: EdgeInsets.all(16.0),
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                elevation: 4,
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Title',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      TextField(
+                        controller: titleController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Title',
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Card(
+                elevation: 4,
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Task Type',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          ChoiceChip(
+                            label: Text('Important'),
+                            selected: selectedTaskType == 'important',
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedTaskType = 'important';
+                              });
+                            },
+                          ),
+                          SizedBox(width: 8),
+                          ChoiceChip(
+                            label: Text('Planner'),
+                            selected: selectedTaskType == 'planner',
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedTaskType = 'planner';
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Card(
+                elevation: 4,
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        controller: descriptionController,
+                        maxLines: 4,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Description',
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Card(
+                elevation: 4,
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Category',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Wrap(
+                        spacing: 8,
+                        children: [
+                          FilterChip(
+                            label: Text('Food'),
+                            selected: selectedCategory == 'food',
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedCategory = 'food';
+                              });
+                            },
+                          ),
+                          FilterChip(
+                            label: Text('Workout'),
+                            selected: selectedCategory == 'workout',
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedCategory = 'workout';
+                              });
+                            },
+                          ),
+                          FilterChip(
+                            label: Text('Run'),
+                            selected: selectedCategory == 'run',
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedCategory = 'run';
+                              });
+                            },
+                          ),
+                          FilterChip(
+                            label: Text('Design'),
+                            selected: selectedCategory == 'design',
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedCategory = 'design';
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: updateTask,
+                  child: Text('Save Changes'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
